3812b529eb87bc4a7e6ed6db60cff7be6b78b5f7
danielzedblock <danielzedblock@gmail.com>
2022-08-31 23:57:34 +0530

    React Project Upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Project</h1>
+      <button>Click Here</button>
+      <Button>Update</Button>
+    </div>
+  );
+}
+
+export default App;
